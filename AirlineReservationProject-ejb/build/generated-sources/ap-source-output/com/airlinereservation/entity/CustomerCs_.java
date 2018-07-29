a09dc01d9c37df5c633607145d613edcd64815c1
Pranav <singarajuvenkatapranav@gmail.com>
2018-07-29 07:23:32 +0530

    first commit

---
+++ AirlineReservationProject-ejb/build/generated-sources/ap-source-output/com/airlinereservation/entity/CustomerCs_.java
@@ -0,0 +1,19 @@
+package com.airlinereservation.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-07-29T00:00:54")
+@StaticMetamodel(CustomerCs.class)
+public class CustomerCs_ { 
+
+    public static volatile SingularAttribute<CustomerCs, String> password;
+    public static volatile SingularAttribute<CustomerCs, String> address;
+    public static volatile SingularAttribute<CustomerCs, String> customerPh;
+    public static volatile SingularAttribute<CustomerCs, Integer> customerId;
+    public static volatile SingularAttribute<CustomerCs, String> name;
+    public static volatile SingularAttribute<CustomerCs, String> email;
+    public static volatile SingularAttribute<CustomerCs, String> ssn;
+
+}
